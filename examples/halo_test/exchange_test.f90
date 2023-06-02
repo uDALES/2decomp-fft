c83c17c98b52b723328df66536f4ae3f06cd016c
Paul Bartholomew <ptb0890@gmail.com>
2023-06-02 16:08:29 +0100

    Test  exchange halo (#6)
    * Clean up the exchange subroutines
    
    Remove unused variables, BUILD=dev now works
    
    * Initially implement exchange halo test
    
    * Only test non-zero halo levels (for now)
    
    Remove some debugging prints
    
    * Don't track test executable
    
    * Initially working test of X exchange
    
    Working on 3 layers.
    
    * Tidying up the exchange test
    
    * Further tidying
    
    * Minor formatting
    
    * Generalising test
    
    * Extend test to Y and Z pencils
    
    * Add global failure test/reporting

---
+++ examples/halo_test/exchange_test.f90
@@ -0,0 +1,225 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! This example calculates the divergence of a random field using
+!   (1) global transposition
+!   (2) halo-cell exchange
+! The two method should give identical results
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+program exchange_test
+
+  use MPI
+  use decomp_2d
+
+  implicit none
+
+  integer, parameter :: nx = 8
+  integer, parameter :: ny = 8
+  integer, parameter :: nz = 8
+  integer :: p_row, p_col
+
+  integer :: ierr
+
+  logical :: passing
+  
+  ! Initialise
+  p_row = 0; p_col = 0
+  passing = .true.
+  
+  call MPI_Init(ierr)
+  if (ierr /= 0) then
+     print *, "ERROR: Initialising MPI failed"
+  end if
+  call decomp_2d_init(nx, ny, nz, p_row, p_col)
+  if (nrank == 0) then
+     print *, "Testing halo exchange routines"
+  end if
+  
+  ! Run tests
+  call test_exchange("X")
+  call test_exchange("Y")
+  call test_exchange("Z")
+
+  if (nrank == 0) then
+     if (.not. passing) then
+        print *, "Exchange test failed"
+        stop 1
+     end if
+  end if
+
+  ! Finalise
+  call decomp_2d_finalize
+  call MPI_Finalize(ierr)
+  if (ierr /= 0) then
+     print *, "ERROR: Finalising MPI failed"
+  end if
+  
+contains
+
+  !! TEST test_exchange
+  !    Tests that the halo exchange in given pencil works.
+  !    Given a pre-allocated array with halo buffers this should fill the halo buffers with the
+  !    correct data.
+  !    Should test a range of halo levels.
+  !    - Corner case: zero halo levels.
+  subroutine test_exchange(orientation)
+
+    character(len=*), intent(in) :: orientation
+    
+    real(mytype), dimension(:,:,:), allocatable :: u
+    integer :: nlevels
+    integer, dimension(3) :: levels
+
+    integer :: xhalo, yhalo, zhalo
+    integer, dimension(3) :: starts, ends, sizes
+
+    logical :: orientation_passing
+
+    xhalo = 1; yhalo = 1; zhalo = 1
+    if (orientation == "X") then
+       xhalo = 0
+       starts = xstart
+       ends = xend
+       sizes = xsize
+    else if (orientation == "Y") then
+       yhalo = 0
+       starts = ystart
+       ends = yend
+       sizes = ysize
+    else if (orientation == "Z") then
+       zhalo = 0
+       starts = zstart
+       ends = zend
+       sizes = zsize
+    else
+       print *, "ERROR: Unknow orientation "//orientation//" test is broken!"
+       stop
+    end if
+    
+    do nlevels = 1, 3
+       levels=(/ xhalo * nlevels, yhalo * nlevels, zhalo * nlevels /)
+       
+       allocate(u(1-levels(1):sizes(1)+levels(1), 1-levels(2):sizes(2)+levels(2), 1-levels(3):sizes(3)+levels(3)))
+
+       call local_init(starts, sizes, levels, u)
+       if (orientation == "X") then
+          call exchange_halo_x(u, opt_xlevel=levels)
+       else if (orientation == "Y") then
+          call exchange_halo_y(u, opt_ylevel=levels)
+       else 
+          call exchange_halo_z(u, opt_zlevel=levels)
+       end if
+       call check(starts, ends, sizes, levels, u, orientation_passing)
+       
+       deallocate(u)
+       
+    end do
+
+    if (nrank == 0) then
+       print *, "+ Orientation: "//orientation
+       if (orientation_passing) then
+          print *, "+- PASS"
+       else
+          print *, "+- FAIL"
+       end if
+    end if
+
+    passing = passing .and. orientation_passing
+    
+  end subroutine test_exchange
+
+  subroutine check(starts, ends, sizes, levels, u, passing)
+
+    integer, dimension(3), intent(in) :: starts, ends, sizes, levels
+    real(mytype), dimension(:,:,:), intent(in) :: u
+    logical, intent(out) :: passing
+    
+    integer :: is, ie, js, je, ks, ke
+    integer :: i, j, k
+    integer :: io, jo, ko
+    integer :: idx
+
+    passing = .true.
+    
+    call valid_range(starts(1), ends(1), sizes(1), nx, levels(1), is, ie)
+    call valid_range(starts(2), ends(2), sizes(2), ny, levels(2), js, je)
+    call valid_range(starts(3), ends(3), sizes(3), nz, levels(3), ks, ke)
+
+    do k = ks, ke
+       ko = k + levels(3)
+       do j = js, je
+          jo = j + levels(2)
+          do i = is, ie
+             io = i + levels(1)
+             call global_index(i, j, k, starts, idx)
+             if (u(io, jo, ko) /= real(idx, mytype)) then
+                print *, "ERROR:", nrank, u(io, jo, ko), real(idx, mytype), idx, i, j, k
+                passing = .false.
+             end if
+          end do
+       end do
+    end do
+
+    call MPI_Allreduce(MPI_IN_PLACE, passing, 1, MPI_LOGICAL, MPI_LAND, MPI_COMM_WORLD, ierr)
+    
+  end subroutine check
+  
+  subroutine local_init(starts, sizes, levels, u)
+
+    integer, dimension(3), intent(in) :: starts, sizes, levels
+    real(mytype), dimension(:,:,:), intent(inout) :: u
+
+    integer :: i, j, k, idx
+    integer :: io, jo, ko
+
+    u(:,:,:) = 0.0_mytype
+    do k = 1, sizes(3)
+       ko = k + levels(3)
+       do j = 1, sizes(2)
+          jo = j + levels(2)
+          do i = 1, sizes(1)
+             io = i + levels(1)
+             call global_index(i, j, k, starts, idx)
+             u(io, jo, ko) = real(idx, mytype)
+          end do
+       end do
+    end do
+    
+  end subroutine local_init
+  
+  subroutine valid_range(pencil_start, pencil_end, pencil_size, nglobal, nlevels, s, e)
+
+    integer, intent(in) :: pencil_start, pencil_end, pencil_size
+    integer, intent(in) :: nglobal
+    integer, intent(in) :: nlevels
+    integer, intent(out) :: s, e
+
+    if (pencil_start == 1) then
+       s = 1
+    else
+       s = 1 - nlevels
+    end if
+
+    if (pencil_end == nglobal) then
+       e = pencil_size
+    else
+       e = pencil_size + nlevels
+    end if
+    
+  end subroutine valid_range
+  
+  subroutine global_index(i, j, k, starts, idx)
+
+    integer, intent(in) :: i, j, k
+    integer, dimension(3), intent(in) :: starts
+    integer, intent(out) :: idx
+
+    integer :: ig, jg, kg
+
+    ig = i + starts(1) - 1
+    jg = j + starts(2) - 1
+    kg = k + starts(3) - 1
+
+    idx = (ig - 1) + (jg - 1) * nx + (kg - 1) * nx * ny
+    
+  end subroutine global_index
+  
+end program exchange_test
